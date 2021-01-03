6ef8df7b183a3801511d74646d93a62e8a8ec883
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-01-03 22:30:02 +0800

    Create basic functions

--- index.js
+++ index.js
@@ -1,16 +1,83 @@
 const RPC = require("discord-rpc");
+const readline = require("readline");
 
 const rpc = new RPC.Client({
     transport: "ipc"
 });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 
 
-rpc.on("ready", () => {
-    rpc.setActivity({
-        details: "Idling",
-        startTimestamp: Date.now()
-    });
+var currentActivity = {};
+
+rl.on("line", (line) => {
+    var args = line.split(" ");
+    var cmd = args.shift();
+
+    if(cmd == "set")
+        setActivity(findArgs(args));
+    else if(cmd == "edit")
+        editActivity(findArgs(args));
+    else if(cmd == "clear")
+        clearActivity();
+    else if(cmd == "idle")
+        setIdle();
+});
+
+process.on("beforeExit", () => { console.log("beforeExit"); clearActivity() });
+
+function findArgs(args) {
+    // var output = {};
+
+    // for(var i = 0; i < args.length; i++) {
+    //     var arg = args[i];
+    // }
+
+    // return output;
+    return { details: "Top Text", state: "Bottom Text", partySize: 3, partyMax: 8 };
+}
+
+rpc.on("ready", () => setIdle());
+rpc.login({ clientId: "794909260257558529" })
+    .then(s => console.log("Logged in."))
+    .catch(e => console.error(e));
+
+
+function updateActivity() {
+    rpc.setActivity(currentActivity).catch((e) => console.log(e));
+}
+
+function clearActivity() {
+    currentActivity = {};
+    rpc.clearActivity();
+}
+
+function setActivity(formattedArgs) {
+    currentActivity = formattedArgs;
+    currentActivity.largeImageKey = "idle";
+    updateActivity();
+}
+
+function editActivity(formattedArgs) {
+    var keys = Object.keys(formattedArgs);
+
+    for(var i = 0; i < keys.length; i++)
+        currentActivity[keys[i]] = formattedArgs[keys[i]];
+
+    updateActivity();
+}
+
+function setIdle() {
+    currentActivity = {
+        largeImageKey: "idle",
+        largeImageText: "Idle", 
+        state: "Idle"
+    };
+    updateActivity();
+}
 
 /*
 static void UpdatePresence()
@@ -29,11 +96,20 @@ static void UpdatePresence()
     discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
     Discord_UpdatePresence(&discordPresence);
 }
+
+-idle
+-meeting
+-class
+-watching
+
+    --discord | --zoom | --meet
+
+    -ts --now (get time now)
+    -ts 1.40.60
+    -ts 2021.1.3.11.40
+    -tr 1.40.60
+        year.month.day.24hour.minute.second
+
+    -top Top Text Here
+    -bot Bottom Text Here
 */
-
-    console.log("Done");
-});
-
-rpc.login({
-    clientId: "794909260257558529"
-});

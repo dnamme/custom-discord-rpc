aee96cb295493b1b32665d52a9a2c27877335a33
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-01-03 13:31:05 +0800

    Add basic functionality

---
+++ index.js
@@ -0,0 +1,39 @@
+const RPC = require("discord-rpc");
+
+const rpc = new RPC.Client({
+    transport: "ipc"
+});
+
+
+
+rpc.on("ready", () => {
+    rpc.setActivity({
+        details: "Idling",
+        startTimestamp: Date.now()
+    });
+
+/*
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Playing Solo";
+    discordPresence.details = "Competitive";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageText = "Numbani";
+    discordPresence.smallImageText = "Rogue - Level 100";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partySize = 1;
+    discordPresence.partyMax = 5;
+    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+    Discord_UpdatePresence(&discordPresence);
+}
+*/
+
+    console.log("Done");
+});
+
+rpc.login({
+    clientId: "794909260257558529"
+});
